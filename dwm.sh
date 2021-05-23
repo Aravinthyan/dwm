8bc5844e10156eb3f5ea4ecf4f6f63d88151166e
Aravinthyan <aravinthyan@outlook.com>
2021-05-23 21:32:03 +0100

    This script will setup some important info and run dwm

---
+++ dwm.sh
@@ -0,0 +1,16 @@
+xset r rate 250 60
+
+feh --bg-fill ~/Pictures/wallpaper.jpg
+
+while xsetroot -name "`date` `uptime | sed 's/.*,//'`"
+do
+	sleep 1
+done &
+
+while true; 
+do
+    dwm >/dev/null 2>&1
+done
+
+exec dwm
+
