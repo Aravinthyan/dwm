004113425eb649be04256fbfbe81543d100c45a0
Aravinthyan <aravinthyan@outlook.com>
2021-05-25 23:48:43 +0100

    Added xset commands to disable blank screen and dpms

--- dwm.sh
+++ dwm.sh
@@ -1,5 +1,14 @@
 xset r rate 250 60
 
+# disable blank
+xset s blank
+xset s 0
+
+# turn off dpms
+xset dpms 0 0 0
+xset -dpms
+
+# set background
 feh --bg-fill ~/Pictures/wallpaper.jpg
 
 sxhkd ~/.config/sxhkd/sxhkdrc &
